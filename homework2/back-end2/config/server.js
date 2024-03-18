c2f21e44a2a73b663d40f5b2f901ff37b22f9f4f
Lucanb <lucanastasa@yahoo.com>
2024-03-18 23:08:01 +0200

    working in react

--- homework2/back-end2/config/server.js
+++ homework2/back-end2/config/server.js
@@ -1,25 +1,69 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import { auth } from "./firebase";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
 const app = express();
 const port = 5000;
 
 app.use(cors());
 
-app.get('/api/todos',(req,res)=>{
-    return res.json({
-        todos: [
-            {
-                title : "task1",
-            },
-            {
-                title : "task2",
-            },
-            {
-                title : "task3",
-            }
-        ]});
+// app.get('/api/todos', (req, res) => {
+//     return res.json({
+//         todos: [
+//             {
+//                 title: "task1",
+//             },
+//             {
+//                 title: "task2",
+//             },
+//             {
+//                 title: "task3",
+//             }
+//         ]
+//     });
+// });
+
+app.post('/signup', async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(error);
+            });
+        res.redirect('/');
+    } catch (e) {
+        res.redirect('signup');
+    }
 });
 
-app.listen(port, ()=>{
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+    res.redirect('/');
+});
+
+app.get('/logout', function (req, res) {
+    auth.signOut().then(() => {
+        res.redirect('/login');
+    }).catch((error) => {
+        // An error happened.
+    });
+});
+
+app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
-})
+});
--- homework2/front-end/src/pages/auth/authenticationDetails.jsx
+++ homework2/front-end/src/pages/auth/authenticationDetails.jsx
@@ -1,35 +1,38 @@
 import React from "react";
 import {useEffect,useState} from "react";
-import {auth} from "../../firebase";
-import {onAuthStateChanged, signOut} from "firebase/auth";
-
+// import {auth} from "../../../../back-end2/config/firebase";
+// import {onAuthStateChanged, signOut} from "firebase/auth";
+//
 const AuthenticationDetails = () =>{
-    const [authUser, setAuthUser] = useState(null);
-    useEffect(()=>{
-        const listen = onAuthStateChanged(auth, (user)=>{
-            if (user){
-                setAuthUser(user);
-            }else{
-                setAuthUser(null);
-            }
-        })
-    },[]);
-    const userSignOut = () =>{
-        signOut(auth).then(()=>{
-            console.log("sign out successful");
-        }).catch(error=>console.log(error));
-    }
+//     const [authUser, setAuthUser] = useState(null);
+//     useEffect(()=>{
+//         const listen = onAuthStateChanged(auth, (user)=>{
+//             if (user){
+//                 setAuthUser(user);
+//             }else{
+//                 setAuthUser(null);
+//             }
+//         })
+//     },[]);
+//     const userSignOut = () =>{
+//         signOut(auth).then(()=>{
+//             console.log("sign out successful");
+//         }).catch(error=>console.log(error));
+//     }
 
     return (
+        // <div>
+        //     {authUser ? (
+        //         <div>
+        //             <p>Signed In</p>
+        //             <button onClick={userSignOut}>Sign Out</button>
+        //         </div>
+        //     ) : (
+        //         <p>Signed Out</p>
+        //     )}
+        // </div>
         <div>
-            {authUser ? (
-                <div>
-                    <p>Signed In</p>
-                    <button onClick={userSignOut}>Sign Out</button>
-                </div>
-            ) : (
-                <p>Signed Out</p>
-            )}
+            <p>Signed Out</p>
         </div>
     );
 }
