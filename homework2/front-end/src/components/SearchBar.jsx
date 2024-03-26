503ed3d2c119287b2622e40149a891cd0214f091
Lucanb <lucanastasa@.com>
2024-03-26 10:47:18 +0200

    Search bar done

--- homework2/front-end/src/components/SearchBar.jsx
+++ homework2/front-end/src/components/SearchBar.jsx
@@ -1,25 +1,33 @@
 import React, { useState } from "react";
 import "./searchBar.css";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchBar({ onSearchResults }) { // Adăugăm proprietatea onSearchResults
     const [input, setInput] = useState("");
-
+    const navigate = useNavigate();
+    const [searchResults, setSearchResults] = useState([]);
     const fetchData = (value) => {
         fetch(`http://localhost:5000/api/artists/getAll`)
             .then((response) => response.json())
             .then((data) => {
-                console.log(data);
+                console.log('data : ',data);
                 const filteredResults = data.filter(
                     (artist) =>
                         artist.name &&
                         artist.name.toLowerCase().includes(value.toLowerCase())
                 );
+                setSearchResults(filteredResults); // Actualizăm starea cu rezultatele căutării
                 onSearchResults(filteredResults); // Apelăm funcția onSearchResults cu rezultatele căutării
             })
             .catch((error) => {
                 console.error("Error fetching data:", error);
             });
+    };
+
+
+    const handleResultClick = (artist) => {
+        navigate(`/artist/${artist._id}`); // Navigare către pagina artistului
     };
 
     const handleChange = (value) => {
@@ -36,6 +44,17 @@ export default function SearchBar({ onSearchResults }) { // Adăugăm proprietat
                     value={input}
                     onChange={(e) => handleChange(e.target.value)}
                 />
+            </div>
+            <div className={"results-list"}>
+                {searchResults.map((result) => (
+                    <div
+                        key={result._id}
+                        className={"search-result"}
+                        onClick={() => handleResultClick(result)}
+                    >
+                        {result.name}
+                    </div>
+                ))}
             </div>
         </div>
     );
--- homework2/front-end/src/pages/Home.jsx
+++ homework2/front-end/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { SearchResult } from "../components/SerchResults"; // Am corectat importul
+// import { SearchResult } from "../components/SerchResults"; // Am corectat importul
 import SearchBar from "../components/SearchBar";
 import PictureList from "../components/PictureList";
 const Home = () => {
-    const [results, setResults] = useState([]);
+    const [setResults] = useState([]);
     const apiUrlArtists = 'http://localhost:5000/api/artists/getAll';
     const apiUrlAlbums = 'http://localhost:5000/api/albums/getAll';
     // Funcție pentru a seta rezultatele căutării în starea componentei
@@ -14,12 +14,12 @@ const Home = () => {
     return (
         <div>
             <h1>Welcome to Music Controller</h1>
-            <SearchBar onSearchResults={handleSearchResults} /> {/* Adăugați o proprietate onSearchResults */}
-            <div className="results-list">
-                {results.map((result, id) => {
-                    return <SearchResult result={result.name} key={id} />;
-                })}
-            </div>
+            <SearchBar onSearchResults={handleSearchResults} />
+            {/*<div className="results-list">*/}
+            {/*    /!*{results.map((result, id) => {*!/*/}
+            {/*    /!*    return <SearchResult result={result.name} key={id} />;*!/*/}
+            {/*    /!*})}*!/*/}
+            {/*</div>*/}
             <h2>Your Albums</h2>
             <PictureList apiUrl={apiUrlAlbums} string={'music'} />
             <h2>Your Artists</h2>
